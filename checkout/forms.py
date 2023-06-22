3289759f776b1af348972b4028fcde1c2627a2be
Joha-will <johawilsonatu@icloud.com>
2023-06-22 01:01:44 +0000

    Created forms.py and added orderform and added signals

---
+++ checkout/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    "Form for users to place orders"
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'country', 'address1', 'address2',
+                  'city', 'county', 'postal_code',)
+
+    def __init__(self, *args, **kwargs):
+        """ Add placeholders and  set autofoucs to first field"""
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'address1': 'Address Line 1',
+            'address2': 'Address Line 2',
+            'city': 'City',
+            'county': 'County',
+            'postal_code': 'Postal Code',
+        }
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
