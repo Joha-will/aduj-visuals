28bccaa5a6fd4b57f85ff2433510c489e7533110
Joha-will <johawilsonatu@icloud.com>
2023-06-22 00:49:02 +0000

    Registered checkout models in admin and created a signals.py file

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('orderitem_total',)
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+    readonly_fields = ('order_number', 'delivery_cost',
+                       'total', 'order_total', 'date_ordered',)
+    fields = ('order_number', 'full_name', 'email',
+              'phone_number', 'address1', 'address2', 'city',
+              'county', 'postal_code', 'delivery_cost', 'total', 'order_total',
+              'date_ordered',)
+    list_display = ('order_number', 'full_name', 'delivery_cost', 'total',
+                    'order_total', 'date_ordered',)
+    ordering = ('date_ordered',)
