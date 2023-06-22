afb338239869a4a5a20091ff97d5ed378f4bc7a7
Joha-will <johawilsonatu@icloud.com>
2023-06-22 15:02:22 +0000

    installed stripe and environments settings

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import (render, redirect, reverse, get_object_or_404,
                               HttpResponse)
 from .forms import OrderForm
+from django.conf import settings
+from basket.contexts import basket_contents
+
+import stripe
 
 
 def checkout(request):
@@ -9,3 +13,6 @@ def checkout(request):
     if not basket:
         messages.error(request, " There's nothing in your basket!")
         return redirect(reverse('products'))
+    current_basket = basket_contents(request)
+    total_amount = current_basket['order_total']
+    stripe_total = round(total_amount * 100)
