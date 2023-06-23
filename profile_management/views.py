a41e088d06fc4fc9b8f5ce517ab578e2cad0389a
Joha-will <johawilsonatu@icloud.com>
2023-06-23 17:30:08 +0000

    Updated allauth template, added placeholder css and create profilemanagement view

--- profile_management/views.py
+++ profile_management/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from django.contrib import messages
 
-# Create your views here.
+
+@login_required
+def view_profile(request):
+    """A view that renders the user profile"""
+    profile = get_object_or_404(UserProfile, user=request.user)
+    template = 'profile_management/view_profile.html'
+    context = {
+        'form': form,
+    }
+    return render(request, template, context)
