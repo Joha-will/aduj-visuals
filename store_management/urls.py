cb7fee45ca7f5f591e2b086d5dceda198f0fa249
Joha-will <johawilsonatu@icloud.com>
2023-06-25 23:50:09 +0000

    Create add_product views, templates, urls, and static files

--- store_management/urls.py
+++ store_management/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('add_product/', views.add_product, name='add_product'),
 
 ]
--- store_management/views.py
+++ store_management/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import (ProductForm)
 
-# Create your views here.
+
+def add_product(request):
+    """ Add's products to the store """
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            product = form.save()
+            messages.info(request, 'Product added successfully!')
+            return redirect(reverse('products'))
+        else:
+            messages.error(request, 'Unable to add product.\
+                            Please check form is valid.')
+    else:
+        form = ProductForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'store_management/add_product.html', context)
