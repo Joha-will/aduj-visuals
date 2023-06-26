fdcd96717321a40f2f5507b66ccc648668b0142e
Joha-will <johawilsonatu@icloud.com>
2023-06-26 00:35:51 +0000

    Added edit product views, template, and urls

--- store_management/urls.py
+++ store_management/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('add_product/', views.add_product, name='add_product'),
+    path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
 
 ]
--- store_management/views.py
+++ store_management/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from .forms import ProductForm
+from products.models import Product, Category
 from django.contrib import messages
 
 
@@ -20,3 +21,26 @@ def add_product(request):
         'form': form,
     }
     return render(request, 'store_management/add_product.html', context)
+
+
+def edit_product(request, product_id):
+    """ Edit product's in the store """
+    product = get_object_or_404(Product, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Product updated successfully.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, 'Unable to update product.\
+                            Please check form is valid.')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing this product\
+             ({ product.name })')
+    context = {
+        'form': form,
+        'product': product,
+    }
+    return render(request, 'store_management/edit_product.html', context)
