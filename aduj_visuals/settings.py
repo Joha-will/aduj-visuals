97d96684ca8ac84a65e5c473d3953a1a385068c1
Joha-will <johawilsonatu@icloud.com>
2023-06-28 12:53:16 +0000

    Adjusted debug settings

--- aduj_visuals/settings.py
+++ aduj_visuals/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['aduj-visuals02.herokuapp.com', 'localhost']
 
@@ -150,6 +150,7 @@ if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
+
 else:
     DATABASES = {
         'default': {
